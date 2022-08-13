5fedb0ab271a69634e7be3aa8fc4fc9529912802
byjus-utsav-majhi <utsav.majhi@byjus.com>
2022-08-14 04:03:41 +0530

    Added material ui and added new homepage design

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import Posts from "../posts/posts";
+import "../../css/Homepage.css";
 
 export default function HomePage() {
-    return (
-        <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
-            <Link to="/">
-                <button className="primary-button">Log out</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+          alt=""
+        />
+      </div>
+      <div className="homepage__postsmain">
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+      </div>
+    </div>
+  );
 }
