9fe48d6f91cfb4bf96f8517be53fc246179e7d21
byjus-ujjwal-giri <ujjwal.giri@byjus.com>
2022-08-14 13:13:04 +0530

    commit3

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,41 +1,67 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Navigate } from 'react-router';
+import axios from "axios";
 
 import "../../App.css";
 
 
 
 export default function SignInPage() {
-
-  const [isTrueUser,setTrueUser] = useState(false);
+  let data ={};
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
 
   const handleSubmit = async e => {
     e.preventDefault();
-    let cred = {email,password}
-    fetch('http://localhost:3000/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify(cred)
-    })
-    .then(res => {
-      if(res.ok){
-        console.log("sccessfully logedin")
-      }
-      else {
-        console.log("wrong credentials")
-      }
-    })
-    .then(data=>console.log(data))
+    let cred = {email : email, password: password}
+    try {
+      const resp = await axios.post(
+        `http://localhost:3000/users/login`,
+        cred,
+        {
+          "content-type": "application/json",
+        }
+      );
+      console.log("Response", JSON.stringify(resp.data));
+      console.log("sccessfully logedin",JSON.stringify(resp.data))
+      data = resp.data
+      setIsLoggedIn(true);
+      
+    } catch (e) {
+      console.log("WRONG CREDENTIALS", e);
+    }
+
+    const iop = {
+      isUserLoggedIn: true/false,
+      userData: data
+    }
+    // fetch('http://localhost:3000/users/login', {
+    // method: 'POST',
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'Accept': 'application/json'
+    // },
+    // body: JSON.stringify(cred)
+    // })
+    // .then(res => {
+    //   if(res.ok){
+    //     console.log("sccessfully logedin",res)
+    //     data = res
+    //     setIsLoggedIn(true);
+    //   }
+    //   else {
+    //     console.log("wrong credentials")
+    //   }
+    // })
  
   }
-
+  if(isLoggedIn) {
+    return (<Navigate to="/home" state={{data:data}}/>);
+  }
   return (
     <div className="text-center m-5-auto">
       <h2>Photo Library</h2>
@@ -65,6 +91,7 @@ export default function SignInPage() {
           </button>
         </p>
       </form>
+      
       <footer>
         <p>
           First time? <Link to="/register">Create an account</Link>.
