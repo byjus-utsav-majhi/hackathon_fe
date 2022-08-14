f428f9626229fbcbfb47a69350e22c1dea081c35
byjus-ujjwal-giri <ujjwal.giri@byjus.com>
2022-08-14 11:02:51 +0530

    commit2

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,17 +1,52 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
 
 import "../../App.css";
 
+
+
 export default function SignInPage() {
+
+  const [isTrueUser,setTrueUser] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    let cred = {email,password}
+    fetch('http://localhost:3000/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(cred)
+    })
+    .then(res => {
+      if(res.ok){
+        console.log("sccessfully logedin")
+      }
+      else {
+        console.log("wrong credentials")
+      }
+    })
+    .then(data=>console.log(data))
+ 
+  }
+
   return (
     <div className="text-center m-5-auto">
       <h2>Photo Library</h2>
-      <form action="/home">
+      <form onSubmit={handleSubmit} action="/home">
         <p>
-          <label>Username or email address</label>
+          <label>email address</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input 
+          type="text" name="email" required
+          onChange={e => setEmail(e.target.value)}
+          />
         </p>
         <p>
           <label>Password</label>
@@ -19,9 +54,12 @@ export default function SignInPage() {
             <label className="right-label">Forget password?</label>
           </Link>
           <br />
-          <input type="password" name="password" required />
+          <input
+          type="password" name="password" required
+          onChange={e => setPassword(e.target.value)}
+          />
         </p>
-        <p>
+        <p >
           <button id="sub_btn" type="submit">
             Login
           </button>
