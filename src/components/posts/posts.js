cd41600f47819d9b0046fff240023e358d40e215
byjus-ujjwal-giri <ujjwal.giri@byjus.com>
2022-08-18 01:36:43 +0530

    commit9

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../css/Post.css";
 import { Avatar } from "@mui/material";
@@ -7,8 +7,17 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import config from "../../config/dev.json";
 import axios from "axios";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Alert from "../dialogs/alert.js";
 
 export default function Posts(props) {
+  const [editDialog,setEditDialog] = useState(false);
+  const [captionDialog, setCaptionDialog] = useState();
   let iop = {};
   if (localStorage.getItem("user")) {
     iop = JSON.parse(localStorage.getItem("user"));
@@ -30,8 +39,64 @@ export default function Posts(props) {
     }
   };
 
+  const handleEditButtonClicked = () => {
+    setEditDialog(true)
+  }
+
+  const handleDialogClose = () => {
+    setEditDialog(false);
+  };
+
+  const handleDialogCaptionChange = (caption) => {
+    setCaptionDialog(caption);
+  };
+
+  const handleUpdateCaption = async (id) => {
+    
+    try {
+      await axios.put(
+        `${config.ruby_host}/posts/update/${id}`, 
+        {
+          caption: captionDialog
+        },
+        {
+          "content-type": "application/json",
+        }
+      )
+      .then(resp => {
+        props.getAll();
+        if(resp.ok){
+        console.log("Response", resp);
+        setEditDialog(false);
+      }});
+    } catch (e) {
+      console.log("Error", e);
+    }
+  }
+
+
   const isOwnPost = props.item.uid === iop.userData.data.uid;
   return (
+    <>
+    <Dialog open={editDialog} onClose={handleDialogClose}>
+        <DialogTitle>Add Post</DialogTitle>
+        <DialogContent>
+          <TextField
+            onChange={(e) => handleDialogCaptionChange(e.target.value)}
+            autoFocus
+            margin="dense"
+            id="caption"
+            label="Post Caption"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={handleUpdateCaption(props.item.id)}>Update Caption</Button>
+        </DialogActions>
+      </Dialog>
     <div className="post">
       <div className="post__header">
         <Avatar
@@ -51,7 +116,10 @@ export default function Posts(props) {
           </div>
         )}
 
-        <IconButton aria-label="edit">
+        <IconButton 
+        aria-label="edit"
+        onClick={handleEditButtonClicked}
+        >
           <EditIcon />
         </IconButton>
       </div>
@@ -63,5 +131,6 @@ export default function Posts(props) {
         {props.item.caption}
       </h4>
     </div>
+    </>
   );
 }
